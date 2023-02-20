87dfe6ffc16a89435945f20fde8f6ce7e48fe520
Yara McRobert <87721861+Roolps@users.noreply.github.com>
2023-02-20 17:51:56 +0000

    Update render.js

--- render.js
+++ render.js
@@ -1,29 +1,67 @@
 // Styling to render (Pure CSS syntax)
 const styling = Object.freeze({
     "initial": {
-        "h1": {
-            "font-size": "20px",
-            "font-weight": "700",
-            "color": "red"
+        "body": {
+            "font-family": "arial"
         },
-        ".test-btn": {
-            "font-size": "10px",
-            "display": "block",
-            "background-color": "orange",
-            "width": "50px",
-            "height": "30px"
+        "p": {
+            "font-size": "12px"
+        },
+        ".btn": {
+            "padding": "10px 20px",
+            "margin-right": "10px",
+            "font-size": "15px",
+            "font-weight": "600",
+            "border": "4px #8a3063 solid",
+            "color": "#8a3063",
+            "cursor": "pointer",
+            "background-color": "transparent",
+            "border-radius": "5px"
+        },
+        ".btn-1": {
+            "color": "#8a3063",
+            "background-color": "transparent"
+        },
+        ".row-flex": {
+            "display": "flex",
+            "flex-direction": "row",
+            "align-items": "center",
+            "margin-bottom": "10px",
+            "width": "400px"
+        },
+        ".block": {
+            "width": "30px",
+            "height": "30px",
+            "background-color": "pink",
+            "margin": "0 0 0 auto"
+        },
+        ".block-1": {
+            "background-color": "pink"
         }
     },
     "animations": {
-        ".animation": {
-            "background-color": "red"
+        ".btn-1": {
+            "background-color": "#8a3063",
+            "color": "#fff"
+        },
+        ".block-1": {
+            "background-color": "#8a3063"
+        },
+        ".block-2": {
+            "background-color": "#8a3063"
         }
     },
     "listeners": {
-        ".test-btn": {
-            "mouseenter": { "animations": [".animation"] },
-            "mouseleave": { "initial": [".test-btn"] },
-            "click": { "animations": [".animation"] }
+        ".btn-1": {
+            "mouseenter": { "animations": [".btn-1"] },
+            "mouseleave": { "initial": [".btn"] }
+        },
+        ".btn-2": {
+            "mouseenter": { "animations": [".block-1"] },
+            "mouseleave": { "initial": [".block-1"] }
+        },
+        ".btn-3": {
+            "mouseenter": { "animations": [".block-2"] }
         }
     }
 });
@@ -31,9 +69,7 @@ const styling = Object.freeze({
 initial();
 
 // Initial style and listener render
-function initial() {
-    Object.keys(styling["initial"]).forEach(key => { style("initial")(key) });
-}
+function initial() { Object.keys(styling["initial"]).forEach(key => { style("initial")(key) }) }
 
 // Renders style..
 function style(type) { return key => { Object.keys(styling[type][key]).forEach(node => { document.querySelectorAll(key).forEach(elem => { elem.style[node] = styling[type][key][node] }) }) } };
